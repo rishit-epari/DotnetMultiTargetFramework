a709e9f9cd238ecac79203e1ec1b30897db86b09
Rishit Epari <epari.rishit@gmail.com>
2022-01-07 13:15:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiTargetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I am able to build and generate the binaries for both Framework and Core!");
+        }
+    }
+}
